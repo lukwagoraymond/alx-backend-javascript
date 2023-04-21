e98b3364cc649d315822a95468874fd25450c9c1
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-04-21 23:48:17 +0300

    Corected Styling

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,18 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+  return Promise.allSettled([signUpPromise, uploadPromise]).then((val) => {
+    const resultarr = [];
+    val.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        resultarr.push({ status: element.status, value: element.value });
+      } else {
+        resultarr.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return resultarr;
+  });
 }
